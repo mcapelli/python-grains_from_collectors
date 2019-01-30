d80eae6e4d96e30b2902efcebdd15f73e55d4cd3
Mauro Capelli <mcapelli@nwnit.com>
2019-01-29 22:52:40 -0500

    discard dic/yaml entries if the role is not = z4Collector

--- src/grains_from_collectors/utility.py
+++ src/grains_from_collectors/utility.py
@@ -5,7 +5,7 @@ def get_collector_name(salt_data):
 
 def get_ip_address(salt_data):
     for key, value in salt_data.items():
-        return value['ip_interfaces']['eth0'][0]
+        return value['ip4_interfaces']['eth0'][0]
 
 
 def get_collector_dicts(input_data):
@@ -14,12 +14,16 @@ def get_collector_dicts(input_data):
         this_dict = {}
         this_dict['hostname'] = key
         try:
-            this_dict['host_ip_address'] = value['ip_interfaces']['eth0'][0]
+            this_dict['host_ip_address'] = value['ip4_interfaces']['eth0'][0]
         except KeyError:
             continue
-        result_list.append(this_dict)
-        # result_list.append(key)
-        # result_list.append(value['ip_interfaces']['eth0'][0])
+        """
+        Discard dict entry if role is not collector
+        """
+        for key in value:
+            if key == 'roles' and value['roles'][0] == 'z4Collector':
+                result_list.append(this_dict)
+
 
     return result_list
 
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -3,6 +3,7 @@ from grains_from_collectors.utility import get_collector_name
 from grains_from_collectors.utility import get_ip_address
 from grains_from_collectors.utility import get_collector_dicts
 from grains_from_collectors.utility import get_data_from_yaml_file
+
 def test_get_collector_name():
     """
     figure out the data format in the salt data and
@@ -13,7 +14,7 @@ def test_get_collector_name():
 
     salt_input_yaml = """
 first_collector_name:
-  ip_interfaces:
+  ip4_interfaces:
     eth0:
       - 99.99.99.99
 """
@@ -31,7 +32,7 @@ def test_get_ip_address():
 
     salt_input_yaml = """
 first_collector_name:
-  ip_interfaces:
+  ip4_interfaces:
     eth0:
       - 99.99.99.99
 """
@@ -49,13 +50,17 @@ def test_get_collector_dicts():
 
     input_yaml = """
     first_collector_name:
-      ip_interfaces:
+      ip4_interfaces:
         eth0:
           - 99.99.99.99
+      roles:
+        - z4Collector 
     second_collector_name:
-      ip_interfaces:
+      ip4_interfaces:
         eth0:
           - 88.88.88.88
+      roles:
+        - z4Collector    
     """
     input_data = yaml.load(input_yaml)
     expected_data = yaml.load(expected_yaml)
@@ -64,6 +69,6 @@ def test_get_collector_dicts():
 
 
 def test_real_data():
-    real = get_data_from_yaml_file('resources/real_data.yml')
+    real = get_data_from_yaml_file('resources/two_item_sample.yml')
     result = get_collector_dicts(real)
-    assert len(result) > 50
+    assert len(result) == 2
